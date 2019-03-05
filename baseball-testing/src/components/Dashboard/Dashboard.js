76868644d0f8e7d9cfddd9f5b71cc70470987d9e
bhumip214 <bhumip214@gmail.com>
2019-03-05 12:59:55 -0800

    Completed functionality of all four buttons

--- baseball-testing/src/components/Dashboard/Dashboard.js
+++ baseball-testing/src/components/Dashboard/Dashboard.js
@@ -4,10 +4,10 @@ function Dashboard(props) {
   return (
     <div>
       <h3> Dashboard</h3>
-      <button> Strike</button>
-      <button> Ball</button>
-      <button> Foul</button>
-      <button> Hit</button>
+      <button onClick={props.handleStrike}> Strike</button>
+      <button onClick={props.handleBall}> Ball</button>
+      <button onClick={props.handleFoul}> Foul</button>
+      <button onClick={props.handleHit}> Hit</button>
     </div>
   );
 }
--- baseball-testing/src/components/Display/Display.js
+++ baseball-testing/src/components/Display/Display.js
@@ -4,8 +4,8 @@ function Display(props) {
   return (
     <div>
       <h3>Counts for At Bat</h3>
-      <p> Balls: {props.balls}</p>
       <p> Strikes: {props.strikes}</p>
+      <p> Balls: {props.balls}</p>
     </div>
   );
 }
