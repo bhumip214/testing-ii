9b3ec2e1b06f798ab4ee7c0eeb962b1955090fc9
bhumip214 <bhumip214@gmail.com>
2019-03-05 18:22:50 -0800

    updated Dashboard testing and added button testing to App test file

--- baseball-testing/src/App.test.js
+++ baseball-testing/src/App.test.js
@@ -1,9 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+describe("<App />", () => {
+  it("increase strikes when clicked on strike button", () => {
+    const { getByTestId } = render(<App />);
+    const strikeButton = getByTestId("strike-btn");
+    const strikes = getByTestId("strikes");
+
+    fireEvent.click(strikeButton);
+    expect(strikes).toHaveTextContent("Strikes: 1");
+  });
+
+  it("increase balls when clicked on ball button", () => {
+    const { getByTestId } = render(<App />);
+    const ballButton = getByTestId("ball-btn");
+    const balls = getByTestId("balls");
+
+    fireEvent.click(ballButton);
+    expect(balls).toHaveTextContent("Balls: 1");
+  });
+
+  it("increase strikes when clicked on foul button", () => {
+    const { getByTestId } = render(<App />);
+    const foulButton = getByTestId("foul-btn");
+    const strikes = getByTestId("strikes");
+
+    fireEvent.click(foulButton);
+    expect(strikes).toHaveTextContent("Strikes: 1");
+  });
+
+  it("reset strikes and balls to 0 when player reaches 3 strikes", () => {
+    const { getByTestId } = render(<App />);
+
+    const strikeButton = getByTestId("strike-btn");
+    const strikes = getByTestId("strikes");
+
+    fireEvent.click(strikeButton);
+    fireEvent.click(strikeButton);
+    expect(strikes).toHaveTextContent("Strikes: 2");
+
+    fireEvent.click(strikeButton);
+    expect(strikes).toHaveTextContent("Strikes: 0");
+  });
+
+  it("reset strikes and balls to 0 when player reaches 4 balls", () => {
+    const { getByTestId } = render(<App />);
+
+    const ballButton = getByTestId("ball-btn");
+    const balls = getByTestId("balls");
+
+    fireEvent.click(ballButton);
+    fireEvent.click(ballButton);
+    fireEvent.click(ballButton);
+    expect(balls).toHaveTextContent("Balls: 3");
+
+    fireEvent.click(ballButton);
+    expect(balls).toHaveTextContent("Balls: 0");
+  });
+
+  it("reset strikes and balls to 0 when clicked on hit button", () => {
+    const { getByTestId } = render(<App />);
+    const hitButton = getByTestId("hit-btn");
+    const strikes = getByTestId("strikes");
+    const balls = getByTestId("balls");
+
+    fireEvent.click(hitButton);
+    expect(strikes).toHaveTextContent("Strikes: 0");
+    expect(balls).toHaveTextContent("Balls: 0");
+  });
 });
--- baseball-testing/src/components/Dashboard/Dashboard.spec.js
+++ baseball-testing/src/components/Dashboard/Dashboard.spec.js
@@ -1,36 +1,19 @@
 import React from "react";
-import { render, fireEvent } from "react-testing-library";
+import { render } from "react-testing-library";
 import "jest-dom/extend-expect";
 import Dashboard from "./Dashboard";
-import Display from "../Display/Display";
 
 describe("<Dashboard />", () => {
   it("should render all four buttons - strikes, balls, fouls, hits", () => {
-    const { getByText } = render(<Dashboard />);
-    expect(getByText(/strike/i)).toBeInTheDocument();
-    expect(getByText(/ball/i)).toBeInTheDocument();
-    expect(getByText(/foul/i)).toBeInTheDocument();
-    expect(getByText(/hit/i)).toBeInTheDocument();
-  });
+    const { getByTestId } = render(<Dashboard />);
+    const strikeButton = getByTestId("strike-btn");
+    const ballButton = getByTestId("ball-btn");
+    const foulButton = getByTestId("foul-btn");
+    const hitButton = getByTestId("hit-btn");
 
-  it("should update strikes and balls on click", () => {
-    const component = render(<Dashboard />);
-    const { getByTestId } = render(<Display strikes={0} balls={0} />);
-
-    const strikeButton = component.getByTestId("strike-btn");
-    const ballButton = component.getByTestId("ball-btn");
-    const foulButton = component.getByTestId("foul-btn");
-    const hitButton = component.getByTestId("hit-btn");
-
-    const strikes = getByTestId("strikes");
-    const balls = getByTestId("balls");
-
-    fireEvent.click(strikeButton);
-    fireEvent.click(ballButton);
-    fireEvent.click(foulButton);
-    fireEvent.click(hitButton);
-
-    expect(strikes).toBeDefined();
-    expect(balls).toBeDefined();
+    expect(strikeButton).toBeInTheDocument();
+    expect(ballButton).toBeInTheDocument();
+    expect(foulButton).toBeInTheDocument();
+    expect(hitButton).toBeInTheDocument();
   });
 });
